f909ea0ab5caf5a74840e4d239c3c53ab2d35731
Hiroaki Yutani <yutani.ini@gmail.com>
2021-10-14 00:55:33 +0900

    Use a dedicated thread

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use extendr_api::graphics::{Context, Device, Unit};
 use extendr_api::prelude::*;
 use extendr_engine::start_r;
 
+use async_std::channel::unbounded;
 use async_std::sync::{Arc, Mutex};
 use tide::prelude::*;
 use tide::Request;
@@ -13,18 +14,23 @@ struct UnsafeDevice(Device);
 
 unsafe impl core::marker::Send for UnsafeDevice {}
 
-#[derive(Clone)]
-struct State {
-    dev: Arc<Mutex<UnsafeDevice>>,
-    svg_file: String,
+impl UnsafeDevice {
+    fn new() -> Self {
+        Self(Device::current().unwrap())
+    }
 }
 
-// Convert JSON inputs into this Rust struct
 #[derive(Debug, Deserialize)]
-struct Circle {
-    x: f64,
-    y: f64,
-    radius: f64,
+#[serde(untagged)]
+enum DeviceOp {
+    PlotCircle { x: f64, y: f64, radius: f64 },
+    WriteResult,
+}
+
+#[derive(Clone)]
+struct State {
+    sender: Arc<Mutex<async_std::channel::Sender<DeviceOp>>>,
+    svg_file: String,
 }
 
 #[async_std::main]
@@ -33,20 +39,63 @@ async fn main() -> tide::Result<()> {
 
     // Use svg() device as this can be displayed on the web browsers
     let dir = std::env::temp_dir();
+
+    // Path for result SVG
     let path = dir.join("test.svg");
     let path_str = path.to_string_lossy().to_string();
+
+    // Path for temporary SVG file
+    let path_tmp = dir.join("tmp.svg");
+    let path_tmp_str = path_tmp.to_string_lossy().to_string();
 
     // For debugging purpose
     println!("{}", &path_str);
 
-    // Create a new device and move it to a `State`
-    R!("svg({{path_str.clone()}})").unwrap();
-    let dev = Device::current().unwrap();
+    // channel for sending requests to the dedicated thread for R operation.
+    let (sender, receiver) = unbounded();
 
     let state = State {
-        dev: Arc::new(Mutex::new(UnsafeDevice(dev))),
-        svg_file: path_str,
+        sender: Arc::new(Mutex::new(sender)),
+        svg_file: path_str.clone(),
     };
+
+    // Create a dedicated thread for calling R function to ensure there's no concurrent R call.
+    let _handle = async_std::task::spawn(async move {
+        loop {
+            println!("Creating a new device...");
+            R!("svg({{path_tmp_str.clone()}})").unwrap();
+            let dev = UnsafeDevice::new();
+
+            while let Ok(op) = receiver.recv().await {
+                match op {
+                    // Plot a circle
+                    DeviceOp::PlotCircle { x, y, radius } => {
+                        println!("Plotting a circle...");
+                        let mut gc = Context::from_device(&dev.0, Unit::Inches);
+
+                        // Draw a circle
+                        gc.fill(Color::rgb(0x20, 0x20, 0xc0));
+                        dev.0.circle((x, y), radius, &gc);
+                    }
+
+                    // Write results to a file and break the while loop
+                    DeviceOp::WriteResult => {
+                        println!("Writing results to file");
+
+                        dev.0.mode_off().unwrap();
+                        R!("dev.off()").unwrap();
+
+                        // The file needs to be moved to allow the new device to open the same name of the file.
+                        // Use an R call instead of Rust in the hope that the IO operation is finished before moving the file (needs confirmation...)
+                        R!("file.rename({{path_tmp_str.clone()}}, {{path_str.clone()}})").unwrap();
+
+                        // Finish the current loop and create a new device
+                        break;
+                    }
+                }
+            }
+        }
+    });
 
     let mut app = tide::with_state(state);
 
@@ -58,26 +107,33 @@ async fn main() -> tide::Result<()> {
 }
 
 async fn plot_point(mut req: Request<State>) -> tide::Result {
-    let Circle { x, y, radius } = req.body_json().await?;
+    if let op @ DeviceOp::PlotCircle { .. } = req.body_json().await? {
+        println!("Request: {:?}", &op);
 
-    let dev = &mut req.state().dev.lock().await.0;
-    let mut gc = Context::from_device(&dev, Unit::Inches);
+        let sender = &mut req.state().sender.lock().await;
 
-    // Draw a circle
-    gc.fill(Color::rgb(0x20, 0x20, 0xc0));
-    dev.circle((x, y), radius, &gc);
+        sender.send(op).await?;
 
-    Ok("OK".into())
+        Ok("OK".into())
+    } else {
+        println!("Invalid request");
+
+        let mut res = tide::Response::new(tide::StatusCode::BadRequest);
+        res.set_body("Invalid JSON");
+        Ok(res)
+    }
 }
 
 async fn plot_result(req: Request<State>) -> tide::Result {
-    let dev = &mut req.state().dev.lock().await.0;
+    let sender = &mut req.state().sender.lock().await;
     let svg_file = req.state().svg_file.clone();
 
-    // Write out the result. Note that, as the device is closed here,
-    // further requests will cause panic.
-    dev.mode_off().unwrap();
-    R!("dev.off()").unwrap();
+    sender.send(DeviceOp::WriteResult).await?;
+
+    // TODO: How many seconds is enough for waiting for R's I/O operations?
+    //       Ideally, this probably needs to be notified from within the dedicated thread.
+    //       But I'm lazy this time.
+    std::thread::sleep(std::time::Duration::from_millis(300));
 
     // Read the result SVG and return it
     let mut res: tide::Response = std::fs::read_to_string(svg_file)?.into();
